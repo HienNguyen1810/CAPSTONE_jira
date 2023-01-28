6d0a5019d643cfe43a46d1c49b3bd64dd3f98499
HienNguyen1810 <nm.hien@tego.global>
2023-01-28 19:56:38 +0700

    update

--- src/pages/LoginFacebook.jsx
+++ src/pages/LoginFacebook.jsx
@@ -6,6 +6,7 @@ import { notification } from 'antd';
 import useAuth from '../hooks/use-auth';
 import userService from '../apis/userApi';
 
+
 function LoginFacebook() {
 	const { setAuth } = useAuth();
 	const navigate = useNavigate();
@@ -28,6 +29,7 @@ function LoginFacebook() {
 					JSON.stringify(res.data.content.accessToken)
 				);
 				localStorage.setItem('email', JSON.stringify(res.data.content.email));
+				localStorage.setItem('idUser', JSON.stringify(values.content.id));
 				setAuth({ email: res.data.content.email });
 				navigate(from, { replace: true, state: '' });
 			});
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import vitePluginImp from 'vite-plugin-imp';
-// import mkcert from 'vite-plugin-mkcert';
+import mkcert from 'vite-plugin-mkcert';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		// mkcert(),
+		mkcert(),
 		vitePluginImp({
 			optimize: true,
 			libList: [
@@ -34,5 +34,5 @@ export default defineConfig({
 			},
 		},
 	},
-	// server: { https: true },
+	server: { https: true },
 });
