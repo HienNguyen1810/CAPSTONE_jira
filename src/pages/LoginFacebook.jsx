99d668848b0e762a8d5459c8a2c1b55f98637af4
HienNguyen1810 <nm.hien@tego.global>
2023-01-28 20:31:28 +0700

    update

--- src/pages/LoginFacebook.jsx
+++ src/pages/LoginFacebook.jsx
@@ -30,6 +30,7 @@ function LoginFacebook() {
 				const email = res.data.content.email;
 				localStorage.setItem('email', JSON.stringify(email));
 				userService.getUserByKeyword(email?.split('.')[0]).then((res) => {
+					console.log(res.data);
 					localStorage.setItem(
 						'idUser',
 						JSON.stringify(res.data.content[0].id)
